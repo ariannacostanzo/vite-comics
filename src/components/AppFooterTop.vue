7f486fb75edb64b4e1eb63412f0ebf6324a329e7
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-01 15:46:03 +0100

    implementing footer top

--- src/components/AppFooterTop.vue
+++ src/components/AppFooterTop.vue
@@ -5,9 +5,40 @@
 </script>
 
 <template>
-  <div>footer top</div>
+  <div id="footer-top">
+    <div class="footer-top container">
+        <nav>
+            <ul>
+                <li>
+                    ciao
+                </li>
+            </ul>
+        </nav>
+        <img src="../assets/images/dc-logo-bg.png" alt="">
+    </div>
+  </div>
 </template>
 
 <style scoped>
-  /* style here */
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+    } 
+
+    #footer-top {
+        padding: 2rem 0;
+        background-image: url(../assets/images/footer-bg.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    .footer-top {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    img {
+        width: 30%;
+    }
 </style>
