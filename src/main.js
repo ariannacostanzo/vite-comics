8704edd8d8ada68b57a00818a33fa3253be6a5ba
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-01 14:38:21 +0100

    scaffolding

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+// import path from 'path'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src')
+//     }
+//   }
+// })
