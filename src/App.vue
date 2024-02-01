792dba322f30f5899bf69dff2a2d400de45b1e4b
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-01 15:29:00 +0100

    adding bonus template

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,18 @@
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
   import AppFooter from './components/AppFooter.vue';
+  import AppBonus from './components/AppBonus.vue';
 
   export default {
     name: 'App',
-    components: {AppHeader, AppMain, AppFooter}
+    components: {AppHeader, AppMain, AppFooter, AppBonus}
   }
 </script>
 
 <template>
   <AppHeader/>
-  <!-- working on main with branch -->
   <AppMain/>
+  <AppBonus/>
   <AppFooter/>
 </template>
 
---
+++ src/components/AppBonus.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+    name: 'AppBonus'
+  }
+</script>
+
+<template>
+    <section>
+
+        <div class="container">content goes here</div>
+    </section>
+</template>
+
+<style scoped>
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+    
+} 
+
+section {
+    color: white;
+    background-color: #0282f9;
+    font-size: 1.5rem;
+    padding: 4rem 0;
+}
+</style>
