8a6083c2fa95bdc0eca76db02883c0b4a052c964
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-02 15:15:05 +0100

    fixing dynamic lists

--- src/components/AppFooterTop.vue
+++ src/components/AppFooterTop.vue
@@ -3,22 +3,27 @@ export default {
     name: 'AppFooterTop',
     data() {
         return {
-            footerNavbar: [
-                {
-                    heading: 'Dc comics',
-                    listItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+            listElements: [
+                {   list: [{
+                        heading: 'Dc comics',
+                        listItems: ['Characters', 'Comics', 'Movies', 'TV', 'Games', 'Videos', 'News']
+                    },
+                    {
+                        heading: 'Shop',
+                        listItems: ['Shop DC', 'Shop DC Collectibles']
+                    }]
                 },
                 {
-                    heading: 'Shop',
-                    listItems: ['Shop DC', 'Shop DC Collectibles']
+                    list: [{
+                        heading: 'DC',
+                        listItems: ['Terms Of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscription', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
+                    }]
                 },
                 {
-                    heading: 'DC',
-                    listItems: ['Terms Of Use', 'Privacy policy (New)', 'Ad Choices', 'Advertising', 'Jobs', 'Subscription', 'Talent Workshops', 'CPSC Certificates', 'Ratings', 'Shop Help', 'Contact Us']
-                },
-                {
-                    heading: 'Sites',
-                    listItems: ['Dc', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+                    list: [{
+                        heading: 'Sites',
+                        listItems: ['Dc', 'MAD Magazine', 'DC Kids', 'DC Universe', 'DC Power Visa']
+                    }]
                 },
 
             ]
@@ -32,53 +37,11 @@ export default {
     <div id="footer-top">
         <div class="footer-top container">
             <nav class="row">
-                <!-- si potrebbe usare ma non riesco a mettere shop sotto dc comics -->
-                <!-- <ul v-for="(ul, i) in footerNavbar" :key="i">
-                    <h4>{{ ul.heading }}</h4>
-                    <li v-for="(li, i) in ul.listItems" :key="i"><a href="#">{{ li }}</a></li>
-                </ul> -->
 
-                <div class="col">
-                    <ul>
-                        <h4>dc comics</h4>
-                        <li><a href="#">Characters</a></li>
-                        <li><a href="#">Comics</a></li>
-                        <li><a href="#">Movies</a></li>
-                        <li><a href="#">TV</a></li>
-                        <li><a href="#">Games</a></li>
-                        <li><a href="#">Videos</a></li>
-                        <li><a href="#">News</a></li>
-                    </ul>
-                    <ul>
-                        <h4>shop</h4>
-                        <li><a href="#">Shop DC</a></li>
-                        <li><a href="#">Shop DC Collectibles</a></li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <ul>
-                        <h4>dc</h4>
-                        <li><a href="#">Terms of Use</a></li>
-                        <li><a href="#">Privacy policy (New)</a></li>
-                        <li><a href="#">Ad Choices</a></li>
-                        <li><a href="#">Advertising</a></li>
-                        <li><a href="#">Jobs</a></li>
-                        <li><a href="#">Subscriptions</a></li>
-                        <li><a href="#">Talent Workshops</a></li>
-                        <li><a href="#">CPSC Certificates</a></li>
-                        <li><a href="#">Ratings</a></li>
-                        <li><a href="#">Shop Help</a></li>
-                        <li><a href="#">Contact us</a></li>
-                    </ul>
-                </div>
-                <div class="col">
-                    <ul>
-                        <h4>sites</h4>
-                        <li><a href="#">DC</a></li>
-                        <li><a href="#">MAD Magazine</a></li>
-                        <li><a href="#">DC Kids</a></li>
-                        <li><a href="#">DC Universe</a></li>
-                        <li><a href="#">DC Power Visa</a></li>
+                <div class="col" v-for="(element, i) in listElements" :key="i">
+                    <ul v-for="(list, i) in element.list" :key="i">
+                        <h4>{{ list.heading }}</h4>
+                        <li v-for="(item, i) in list.listItems" :key="i"><a href="#">{{ item }}</a></li>
                     </ul>
                 </div>
 
